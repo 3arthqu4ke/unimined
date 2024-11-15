cc72f87918958f65457632dc35277567db30229b
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2024-11-15 16:11:14 +0100

    Show that 1.12.2 is broken due to remapSources task

---
+++ testing/1.12.2-Forge-Fabric/src/main/java/com/example/mixins/MixinMinecraft.java
@@ -0,0 +1,17 @@
+package com.example.mixins;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public abstract class MixinMinecraft {
+    @Inject(method = "displayGuiScreen", at = @At("HEAD"))
+    private void displayGuiScreenHook(GuiScreen guiScreenIn, CallbackInfo ci) {
+
+    }
+
+}
